bc7becf20111381d7e7a80775f55450e998f9952
xsank.mz <xsank.mz@alibaba-inc.com>
2016-07-10 15:20:50 +0800

    ajust the ui structure

---
+++ static/js/main.js
@@ -0,0 +1,67 @@
+function openTerminal(options) {
+    var client = new WSSHClient();
+    var term = new Terminal({cols: 80, rows: 24, screenKeys: true});
+    term.on('data', function (data) {
+        client.sendClientData(data);
+    });
+    term.open();
+    $('.terminal').detach().appendTo('#term');
+    term.write('Connecting...');
+    client.connect({
+        onError: function (error) {
+            term.write('Error: ' + error + '\r\n');
+            console.debug('error happened');
+        },
+        onConnect: function () {
+            client.sendInitData(options);
+            client.sendClientData('\r');
+            console.debug('connection established');
+        },
+        onClose: function () {
+            term.write("\rconnection closed")
+            console.debug('connection reset by peer');
+        },
+        onData: function (data) {
+            term.write(data);
+            console.debug('get data:' + data);
+        }
+    })
+}
+
+function store(options) {
+    window.localStorage.host = options.host
+    window.localStorage.port = options.port
+    window.localStorage.username = options.username
+    window.localStorage.password = options.password
+}
+
+function check() {
+    var result = $("#host").val() && $("#port").val() && $("#username").val() && $("#password").val()
+    if (result) {
+        var spans = $("fieldset").find("span")
+        for (var i = 0; i < spans.length; i++) {
+            if (spans[i].innerHTML.trim() != "correct") {
+                return false
+            }
+        }
+    }
+    return result
+}
+
+function connect() {
+    var remember = $("#remember").is(":checked")
+    var options = {
+        host: $("#host").val(),
+        port: $("#port").val(),
+        username: $("#username").val(),
+        password: $("#password").val()
+    }
+    if (remember) {
+        store(options)
+    }
+    if (check()) {
+        openTerminal(options)
+    } else {
+        alert("please check the form!")
+    }
+}
--- static/js/ws.js
+++ static/js/ws.js
@@ -1,7 +1,7 @@
 function WSSHClient() {
 };
 
-WSSHClient.prototype._generateEndpoint = function() {
+WSSHClient.prototype._generateEndpoint = function () {
     if (window.location.protocol == 'https:') {
         var protocol = 'wss://';
     } else {
@@ -11,7 +11,7 @@ WSSHClient.prototype._generateEndpoint = function() {
     return endpoint;
 };
 
-WSSHClient.prototype.connect = function(options) {
+WSSHClient.prototype.connect = function (options) {
     var endpoint = this._generateEndpoint();
 
     if (window.WebSocket) {
@@ -22,40 +22,40 @@ WSSHClient.prototype.connect = function(options) {
     }
     else {
         options.onError('WebSocket Not Supported');
-        return ;
+        return;
     }
 
-    this._connection.onopen = function() {
+    this._connection.onopen = function () {
         options.onConnect();
     };
 
     this._connection.onmessage = function (evt) {
-        var data=evt.data.toString()
+        var data = evt.data.toString()
         options.onData(data);
     };
 
 
-    this._connection.onclose = function(evt) {
+    this._connection.onclose = function (evt) {
         options.onClose();
     };
 };
 
-WSSHClient.prototype.send = function(data) {
+WSSHClient.prototype.send = function (data) {
     this._connection.send(JSON.stringify(data));
 };
 
-WSSHClient.prototype.sendInitData=function(options){
-    var data= {
+WSSHClient.prototype.sendInitData = function (options) {
+    var data = {
         hostname: options.host,
         port: options.port,
         username: options.username,
         password: options.password
     }
-    this._connection.send(JSON.stringify({"tp":"init","data":data}))
+    this._connection.send(JSON.stringify({"tp": "init", "data": data}))
 }
 
-WSSHClient.prototype.sendClientData=function(data){
-    this._connection.send(JSON.stringify({"tp":"client","data":data}))
+WSSHClient.prototype.sendClientData = function (data) {
+    this._connection.send(JSON.stringify({"tp": "client", "data": data}))
 }
 
 var client = new WSSHClient();
